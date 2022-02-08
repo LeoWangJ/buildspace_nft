2726aa349a877850897f63843f3ce44b8348d423
leowang <rfv7855659@gmail.com>
2022-02-09 01:56:53 +0800

    ✨add event

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,73 +5,128 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-import { Base64 } from "./libraries/Base64.sol";
+import {Base64} from "./libraries/Base64.sol";
 
 contract NFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+    string svgPartOne =
+        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
+    string svgPartTwo =
+        "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
-    string[] firstWords = ["EARN", "LOSE","GIVE","WIN","FORGET","GIVEUP"];
-    string[] secondWords = ["0","10","100","1000","10000","100000"];
-    string[] thirdWords = ["BTC", "ETH","SOL","BNB","MATIC","USDT"];
+    string[] firstWords = ["EARN", "LOSE", "GIVE", "WIN", "FORGET", "GIVEUP"];
+    string[] secondWords = ["0", "10", "100", "1000", "10000", "100000"];
+    string[] thirdWords = ["BTC", "ETH", "SOL", "BNB", "MATIC", "USDT"];
+    string[] colors = ["red", "#08C2A8", "black", "yellow", "blue", "green"];
 
-    constructor() ERC721("SquareNFT","SQUARE") {
+    event NewEpicNFTMinted(address sender, uint256 tokenId);
+
+    constructor() ERC721("SquareNFT", "SQUARE") {
         console.log("This is my NFT contract!");
     }
 
-    function makeAnNFT () public {
-        uint newItemId = _tokenIds.current();
+    function makeAnNFT() public {
+        uint256 newItemId = _tokenIds.current();
         string memory first = pickRandomFirstWord(newItemId);
         string memory second = pickRandomSecondWord(newItemId);
         string memory third = pickRandomThirdWord(newItemId);
-        string memory combinedWord = string(abi.encodePacked(first,second,third));
-        string memory finalSvg = string(abi.encodePacked(baseSvg,combinedWord,"</text></svg>"));
-        
-        string memory json = Base64.encode(
-            bytes(string(abi.encodePacked(
-                '{"name": "',
-                // We set the title of our NFT as the generated word.
-                combinedWord,
-                '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
-                // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
-                Base64.encode(bytes(finalSvg)),
-                '"}'
-            )))
+        string memory combinedWord = string(
+            abi.encodePacked(first, second, third)
         );
-        string memory finalTokenUri = string(abi.encodePacked("data:application/json;base64,", json));
+        string memory randomColor = pickRandomColor(newItemId);
+        string memory finalSvg = string(
+            abi.encodePacked(
+                svgPartOne,
+                randomColor,
+                svgPartTwo,
+                combinedWord,
+                "</text></svg>"
+            )
+        );
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        // We set the title of our NFT as the generated word.
+                        combinedWord,
+                        '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
+                        // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
+                        Base64.encode(bytes(finalSvg)),
+                        '"}'
+                    )
+                )
+            )
+        );
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
         console.log("\n--------------------");
         console.log(finalTokenUri);
         console.log("--------------------\n");
 
         _safeMint(msg.sender, newItemId);
         _setTokenURI(newItemId, finalTokenUri);
-        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+        console.log(
+            "An NFT w/ ID %s has been minted to %s",
+            newItemId,
+            msg.sender
+        );
         _tokenIds.increment();
+        emit NewEpicNFTMinted(msg.sender, newItemId);
     }
 
-    function random(string memory input) internal pure returns(uint256) {
+    function random(string memory input) internal pure returns (uint256) {
         return uint256(keccak256(abi.encodePacked(input)));
     }
 
-    function pickRandomFirstWord(uint tokenId) public view returns(string memory){
-        uint rand = random(string(abi.encodePacked("FIRST_WORD",Strings.toString(tokenId))));
+    function pickRandomFirstWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId)))
+        );
         rand = rand % firstWords.length;
         return firstWords[rand];
     }
-    
 
-    function pickRandomSecondWord(uint tokenId) public view returns(string memory){
-         uint rand = random(string(abi.encodePacked("SECOND_WORD",Strings.toString(tokenId))));
+    function pickRandomSecondWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId)))
+        );
         rand = rand % secondWords.length;
         return secondWords[rand];
     }
 
-    function pickRandomThirdWord(uint tokenId) public view returns(string memory){
-         uint rand = random(string(abi.encodePacked("THIRD_WORD",Strings.toString(tokenId))));
+    function pickRandomThirdWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId)))
+        );
         rand = rand % thirdWords.length;
         return thirdWords[rand];
     }
 
-
+    function pickRandomColor(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("COLOR", Strings.toString(tokenId)))
+        );
+        rand = rand % colors.length;
+        return colors[rand];
+    }
 }
